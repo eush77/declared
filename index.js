95912a670cdbfe7dbdd4cf56edd83b898cd70a32
Eugene Sharygin <eush77@gmail.com>
2014-11-16 19:12:00 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+module.exports = function () {
+  for (var i = 0; i < arguments.length; ++i) {
+    if (arguments[i] != null) {
+      return arguments[i];
+    }
+  }
+  return undefined;
+};
